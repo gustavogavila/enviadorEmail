fda8c4291eb2d3c93f6860f96c7179a348dbcae7
Gustavo Guimarães Ávila <gustavogavila01@gmail.com>
2018-05-15 17:06:50 -0300

    exportacao do projeto completo

---
+++ src/main/java/br/com/gustavogavila/enviadorEmail/enviadorEmail/EmailService.java
@@ -0,0 +1,33 @@
+package br.com.gustavogavila.enviadorEmail.enviadorEmail;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+	
+	public void enviar(String nome, String emailConvidado) {
+		
+		try {
+			Email email = new SimpleEmail();
+			// Preencher com as informações do seu serviço de e-mail
+			email.setHostName("smtp-mail.outlook.com");
+			email.setSmtpPort(587);
+			email.setAuthenticator(new DefaultAuthenticator("seuemail@servico.com", "suaSenha"));
+			email.setStartTLSRequired(true);
+			email.setStartTLSEnabled(true);
+			
+			email.setFrom("seuemail@servico.com");
+			email.setSubject("Assunto do email");
+			email.setMsg("Olá " + nome + ", você foi convidado para nomeDaAplicação!");
+			email.addTo(emailConvidado);
+			email.send();
+		
+		} catch (EmailException e) {
+			e.printStackTrace();
+		}
+	}
+}
